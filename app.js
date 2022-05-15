c63f1d14640ce23aff149039262952d35c3d47e6
poyang31 <poyang31@yahoo.com.tw>
2022-05-15 21:14:54 +0800

    update for http_status

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 
 require('dotenv').config();
 
-const
-    http_status = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
 const
     constant = require('./src/init/const'),
@@ -19,18 +18,27 @@ const
     },
     schema = {},
     middleware = {
+        file_upload: require('express-fileupload'),
         access: require('./src/middlewares/access'),
     };
 
 const app = require('./src/init/express')(ctx);
 
 app.get('/', (req, res) => {
-    res.redirect(http_status.MOVED_PERMANENTLY, process.env.WEBSITE_URL);
+    res.redirect(StatusCodes.MOVED_PERMANENTLY, process.env.WEBSITE_URL);
 });
 
 app.get('/ip', (req, res) => {
     res.send({ip_address: util.ip_address(req)});
 });
+
+app.post('/house/photo', middleware.file_upload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+}), (req, res) => {
+    if (!req.files?.file) {
+        res.sendStatus(StatusCodes.BAD_REQUEST)
+    }
+})
 
 
 app.listen(process.env.HTTP_PORT, process.env.HTTP_HOSTNAME, () => {
