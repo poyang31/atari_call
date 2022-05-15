970fcd7fbc7088a1478f069448cb04dbef1a52d6
aunne <unihae236@gmail.com>
2022-05-15 21:17:12 +0800

    update schemas

--- src/schemas/Article.js
+++ src/schemas/Article.js
@@ -7,4 +7,10 @@ const ObjectId = Schema.ObjectId;
 
 module.exports = new Schema({
     id: ObjectId,
+    local: String, //地區
+    price: Number, //房租
+    contant: Object, //聯絡方式
+    people: Number, //人數
+    idFound: Boolean, //是否已找到
+    condition: Array //房屋條件
 });
--- src/schemas/House.js
+++ src/schemas/House.js
@@ -1,13 +1,18 @@
 "use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 module.exports = new Schema({
     id: ObjectId,
-    price: Number,
-    address: String,
-    contact: Object,
+    price: Number, // 房租
+    photo: String, // 照片
+    isSold: Boolean, //是否租出
+    address: String, //地址
+    people: Number, //人數
+    contact: Object, //聯絡方式
+    soldDate: Number, //出租日期
+    condition: Array //房屋條件
 });
